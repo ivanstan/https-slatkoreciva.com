1c63b078834820b93a03a331ac7e568284b83eee
Ivan Stanojevic <ivan@byteout.com>
2019-09-02 19:42:02 +0200

    Initial dist subtree commit

---
+++ build/precache-manifest.4cf02b4bbd0f8a47fec847a57f169693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a789798ff2f93af73c4454fa4e50e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c29e52c3aa902bd95702",
+    "url": "/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "3f02c789a8144e67c3cb",
+    "url": "/static/css/main.25c4c511.chunk.css"
+  },
+  {
+    "revision": "c29e52c3aa902bd95702",
+    "url": "/static/js/2.0abe1e97.chunk.js"
+  },
+  {
+    "revision": "3f02c789a8144e67c3cb",
+    "url": "/static/js/main.8f399dc9.chunk.js"
+  },
+  {
+    "revision": "5d4172d5b547a58f2f0c",
+    "url": "/static/js/runtime~main.ab2510d2.js"
+  }
+]);
